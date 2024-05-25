7099994e3e89d0c4947807a1a3108ba0e272fda8
Komalmsaleh <160410944+Komalmsaleh@users.noreply.github.com>
2024-05-25 20:40:01 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer"
+
+
+
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "please enter your sentence"
+})
+
+
+ let word_count = user_ans.words.trim().spilt(" ").length
+ 
+ console.log(`Your sentence has ${word_count} words.`);
